a16c031745feb5ab5997ef678540fe5895e2390f
Alex Bryan <alexsbryan@gmail.com>
2015-05-18 23:14:45 -0700

    adding view delegate and some layers to get ready for swiping gesture

--- Phabric Later/TaskTableViewCell.swift
+++ Phabric Later/TaskTableViewCell.swift
@@ -1,10 +1,17 @@
 import UIKit
 
+@objc protocol TaskTableViewCellDelegate {
+    func taskTableViewCell(taskTableViewCell: TaskTableViewCell, didSwipe value: Bool)
+}
+
 class TaskTableViewCell: UITableViewCell {
 
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var avatarImageView: UIImageView!
+    weak var delegate: TaskTableViewCellDelegate?
     
+    @IBOutlet weak var topBackgroundView: UIView!
+    @IBOutlet weak var bottomBackgroundView: UIView!
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
@@ -13,5 +20,8 @@ class TaskTableViewCell: UITableViewCell {
     override func setSelected(selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
     }
+    
+    //call the delegate function didSwipe on the Ended part of the
+    //swipe action
 
 }
